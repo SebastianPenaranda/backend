3576a5e2cf17624c249e5b14fb00f6db35e835cb
Sebastian Peñaranda <penarandagrijalbasebastian@gmail.com>
2025-05-11 00:56:49 -0500

    Correcion de correo y privacidad en consola

--- mailer.js
+++ mailer.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_APP_PASSWORD // Usar contraseña de aplicación de Gmail
+        pass: process.env.EMAIL_PASSWORD // Usar contraseña de aplicación de Gmail
     }
 });
 
